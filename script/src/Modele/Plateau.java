d404d59e9d3304f6460e5327c77eb9181c6438c5
PUSKAS Matyas <matyaspuskas76@protonmail.com>
2022-02-10 15:33:19 +0100

    correction

--- script/src/Modele/Plateau.java
+++ script/src/Modele/Plateau.java
@@ -22,60 +22,29 @@ public class Plateau {
 
 
 
-	public static void produitMatrices(int mat[][], int x1[],int x2[]){
-		int Matx1x2[][]= new int [2][2];
-		int produit[][]= new int [2][2];
-		System.out.println("Matrice 1 : ");
-		for (int i=0; i< mat.length;i++){
-			for (int j =0; j<mat.length;j++){
-				System.out.print(mat[i][j]+" ");
-			}
-			System.out.println("\t");
-		}
+	public static int[] produitMatrices(int mat[][], int x1,int x2){
+		int produit[] = new int [2];
 
-		//Calcul produit
-		System.out.println("Matrice 2 : ");
-		for (int i=0; i< 2;i++){
-			for (int j =0; j<2;j++){
-				Matx1x2[0][j]=x1[j];
-				Matx1x2[1][j]=x2[j];
-				System.out.print(Matx1x2[i][j]+" ");
-
-			}
-			System.out.println("\t");
-		}
-		System.out.println("Produit Matrice 1 et Matrice 2 : ");
-		for (int i=0; i< 2;i++){
-			for (int j =0; j<2;j++){
-				produit[i][j] = 0;
-				for( int k = 0 ; k < 2 ; k ++ )
-					produit[i][j] = produit[i][j] + mat[i][k] * Matx1x2[k][j] ;
-				System.out.print(produit[i][j]+" ");
-
-			}
-			System.out.println("\t");
-		}
+		produit[0] = mat[0][0]*x1 + mat[0][1]*x2;
+		produit[1] = mat[1][0]*x1 + mat[1][1]*x2;
+		return produit;
 
 	}
 
 	public static void main(String[] args) {
 
-		int x1[]= {-1,4};
-		int x2[]={7,-6};
+		int x1= 2;
+		int x2= 3;
 		int mat[][]={{2,-2},{5,3}};
-		produitMatrices(mat, x1, x2);
-
-
+		System.out.println(produitMatrices(mat, x1, x2)[0] + "\n" + produitMatrices(mat, x1, x2)[1]);
 	}
+
 	public Case getCase(int x, int y){
 		return plateau[x][y];
 	}
+
 	public void setCase(int x, int y,Case c){
 			plateau[x][y] = c;
-	}
-
-	public boolean deplacerBloc() {
-		return true;
 	}
 	
 	public void init(){
