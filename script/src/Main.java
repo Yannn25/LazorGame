dede1b67a901005a1b7e61dea8ea64ec9272c3d4
Yann EDIKEU <yann94edikeu@gmail.com>
2022-05-12 16:10:08 +0200

    ajout commande pour sauvegarder tous les niveaux java

--- script/src/Main.java
+++ script/src/Main.java
@@ -6,6 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
         /*try{
+            Controleur.initNiveau1().sauvegarder("Niveau1");
+            Controleur.initNiveau2().sauvegarder("Niveau2");
+            Controleur.initNiveau3().sauvegarder("Niveau3");
+            Controleur.initNiveau4().sauvegarder("Niveau4");
+            Controleur.initNiveau5().sauvegarder("Niveau5");
             Controleur.initNiveau6().sauvegarder("Niveau6");
         }
         catch(Exception e){
