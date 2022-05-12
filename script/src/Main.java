e2d3668550046f004623f6ccd62b32e142a5cfb9
Yann EDIKEU <yann94edikeu@gmail.com>
2022-05-12 13:44:06 +0200

    resérialisation des niveau

--- script/src/Main.java
+++ script/src/Main.java
@@ -3,12 +3,12 @@ import controleur.Controleur;
 public class Main {
 
     public static void main(String[] args){
-        /*try{
-            Controleur.initNiveau3().sauvegarder("Niveau3");
+        try{
+            Controleur.initNiveau6().sauvegarder("Niveau6");
         }
         catch(Exception e){
             e.printStackTrace();
-        }*/
+        }
         Controleur controleur = new Controleur();
         controleur.commencerJeu();
     }
